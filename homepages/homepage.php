400bdff02122b728e248a26a65c637dcd34eb30c
root <root@samuel>
2023-06-24 01:19:31 +0300

    homepage file

---
+++ homepages/homepage.php
@@ -0,0 +1,11 @@
+<?php require_once "../header-footer/header.php";?>
+<DOCTYPE html>
+  <head>
+    <title>Kibwona Miracle Centre Church</title>
+  </head>
+  <body>
+    <h1>KIBWONA MIRACLE CENTRE CHURCH</h1>
+    <h1>LEADING ALL PEOPLE TO FIND LIFE + FREEDOM IN JESUS<h1>
+    <h1>Pastor Kajura Julius Ministries</h1>
+  </body>
+</html>
