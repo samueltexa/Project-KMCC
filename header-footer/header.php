d64d6fb7b2b4508e4ef76e5da70edf4919ce94c4
root <root@samuel>
2023-06-24 01:18:36 +0300

    adding header-footer

---
+++ header-footer/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../sourcefiles/custom/custom.css">
+</head>
+<body>
+	<div class="header">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Ministries</a></li>
+			<li><a href="#">Activities</a></li>
+			<li><a href="#">Location</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Our Projects</a></li>
+			<li><a href="#">Online Giving</a></li>
+      <li><a href="#">Donations</a></li>
+      <li><a href="#">Watch Online Service</a></li>
+		</ul>
+  </div>
+	</body>
+</html>
