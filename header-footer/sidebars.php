54686ffa1ad210a70291974f97918380ccd6327e
root <root@samuel>
2023-07-09 15:52:45 +0300

    side bars

---
+++ header-footer/sidebars.php
@@ -0,0 +1,9 @@
+<DOCTYPE html>
+  <head>
+    <link rel="stylesheet" href="../sourcefiles/CSS/sidebars.css">
+  </head>
+  <body>
+      <div class="sidebar sidebar-left"></div>
+      <div class="sidebar sidebar-right"></div>
+  </body>
+</html>
