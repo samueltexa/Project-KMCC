1129e11c49da840d31b5b43f8b423449473d9437
root <root@samuel>
2023-07-09 15:55:28 +0300

    modified files

--- header-footer/header.php
+++ header-footer/header.php
@@ -1,23 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../sourcefiles/custom/custom.css">
-</head>
-<body>
-	<div class="header">
-		<ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">Ministries</a></li>
-			<li><a href="#">Activities</a></li>
-			<li><a href="#">Location</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Our Projects</a></li>
-			<li><a href="#">Online Giving</a></li>
-      <li><a href="#">Donations</a></li>
-      <li><a href="#">Watch Online Service</a></li>
-		</ul>
-  </div>
-	</body>
+  <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="../sourcefiles/CSS/header.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+      <div class="header">
+      	<a href="#">News</a>
+      	<a href="#">Events</a>
+      	<a href="#">Upcoming</a>
+      	<a href="#">Galleries</a>
+      	<a href="#">Forums</a>
+      </div>
+    </body>
 </html>
--- homepages/homepage.php
+++ homepages/homepage.php
@@ -1,11 +1,44 @@
-<?php require_once "../header-footer/header.php";?>
+<?php require_once "../header-footer/topnav.php";?>
 <DOCTYPE html>
   <head>
     <title>Kibwona Miracle Centre Church</title>
+    <link rel="stylesheet" href="../sourcefiles/CSS/images.css">
+    <link rel="stylesheet" href="../sourcefiles/CSS/content.css">
   </head>
+
   <body>
-    <h1>KIBWONA MIRACLE CENTRE CHURCH</h1>
+
+<?php require_once "../header-footer/sidebars.php";?>
+<canvas id="myCanvas" class="myCanvas">
+  Your browser does not support the HTML canvas tag.
+</canvas>
+  </div>
+  <div>
+<?php require_once "../header-footer/header.php";?>
+</div>
+  <div class="content">
+    <h1>KMCC CHURCH</h1>
     <h1>LEADING ALL PEOPLE TO FIND LIFE + FREEDOM IN JESUS<h1>
     <h1>Pastor Kajura Julius Ministries</h1>
+    <h1>Once upon a time in a small town, there was a humble church called Harmony Chapel. The church had a long-standing tradition of welcoming everyone with open arms, regardless of their background or beliefs. It was known as a place of love, compassion, and acceptance.
+The pastor of Harmony Chapel, Pastor James, was a wise and kind-hearted individual who deeply cared for his congregation. He believed in the power of unity and sought to create a community that fostered understanding and harmony among its members.
+One day, a young woman named Sarah arrived in town. She had recently experienced a difficult time in her life and was searching for solace and guidance. Feeling lost and alone, she decided to visit Harmony Chapel.
+As Sarah stepped into the church, she was greeted by warm smiles and genuine kindness. The church members made her feel welcomed and included from the very beginning. Pastor James, with his gentle demeanor, approached Sarah and offered a listening ear.
+Over the following weeks, Sarah attended the church's services and events. She found solace in the prayers, the uplifting music, and the heartfelt sermons. What touched her the most was the genuine care and support she received from the church community. The church members became her friends and extended family, providing her with the love and encouragement she desperately needed.
+Inspired by the church's atmosphere of inclusivity and compassion, Sarah decided to give back to the community. She volunteered at the church's outreach programs, helping those in need and spreading kindness wherever she went. Witnessing the positive impact she was making, Sarah's own spirit was uplifted, and she found a renewed sense of purpose and fulfillment.
+As time passed, more individuals like Sarah found their way to Harmony Chapel. The church became a beacon of hope and refuge for people from all walks of life. It embraced diversity and celebrated the unique gifts and talents each person brought to the community.
+Together, the members of Harmony Chapel worked towards making a difference in the town. They organized food drives, supported local charities, and reached out to the marginalized members of society. Their collective efforts touched the lives of many, bringing about positive change and fostering a sense of unity within the community.
+The story of Harmony Chapel spread far and wide, and people started recognizing the transformative power of love and acceptance. The church's influence extended beyond its physical walls, inspiring other communities to embrace similar values of compassion and understanding.
+And so, the story of Harmony Chapel serves as a reminder of the profound impact a church can have when it embraces all individuals, offers support and guidance, and becomes a catalyst for positive change in the world.</h1>
+  </div>
+  <script>
+    let canvas = document.getElementById('myCanvas');
+    let ctx = canvas.getContext('2d');
+    let newImage = new Image();
+    newImage.src ='../images/default.png';
+    newImage.onload = () => {
+      ctx.drawImage(newImage, 0, 0, canvas.width, canvas.height);
+    }
+  </script>
   </body>
 </html>
